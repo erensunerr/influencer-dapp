ce3d5f911ced8ad58d5216df83299dfb4c85daa7
Eren Suner <gerensuner@gmail.com>
2019-11-09 14:32:39 -0500

    init w3

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
-
-
+import {Switch, Route} from "react-router-dom";
+import Business from './pages/business'
+import Influencer from './pages/influencer'
 function App() {
   return (
     <div className="App">
-
+      <Switch>
+        <Route path="/influencer">
+          <Influencer />
+        </Route>
+        <Route path="/business">
+          <Business />
+        </Route>
+      </Switch>
     </div>
   );
 }
---
+++ src/Contract.js
@@ -0,0 +1,11 @@
+const Web3 = require('web3')
+const web3 = new Web3('')
+export default Contract {
+  function constructor(influencerAddress, username, adQuota, hashtag) {
+    
+  }
+  function deploy() {
+
+  }
+}
+//PREPARE FOR THE WORST
