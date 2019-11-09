d69e4764e1199cb164a2ea8bd686af0a060ed813
Eren Suner <gerensuner@gmail.com>
2019-11-09 15:31:55 -0500

    truffle init

--- src/Contract.js
+++ src/Contract.js
@@ -1,8 +1,7 @@
-const Web3 = require('web3')
-const web3 = new Web3('')
+
 export default Contract {
   function constructor(influencerAddress, username, adQuota, hashtag) {
-    
+
   }
   function deploy() {
 
